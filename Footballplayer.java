0e54aff6b545faee485bfb69c11a778167303609
Sanchari Shakti <shaktikarmakar272@gmail.com>
2026-03-04 19:37:28 +0600

    Update Footballplayer.java

--- Footballplayer.java
+++ Footballplayer.java
@@ -1,7 +1,7 @@
-abstract class FootballPlayer {
+public abstract class FootballPlayer {
     protected String name;
     protected int goals;
-    private double monthlySalary; 
+    private double monthlySalary; // Confidential
 
     public FootballPlayer(String name, int goals, double monthlySalary) {
         this.name = name;
@@ -14,44 +14,5 @@ abstract class FootballPlayer {
     public void displayBasicInfo() {
         System.out.println("Name: " + name);
         System.out.println("Goals Scored: " + goals);
-    }
-}
-
-class LaLigaPlayer extends FootballPlayer {
-    public LaLigaPlayer(String name, int goals, double salary) {
-        super(name, goals, salary);
-    }
-
-    @Override
-    public double calculateGoalRate(int matchesPlayed) {
-        return (double) goals / matchesPlayed;
-    }
-}
-
-class PremierLeaguePlayer extends FootballPlayer {
-    public PremierLeaguePlayer(String name, int goals, double salary) {
-        super(name, goals, salary);
-    }
-
-    @Override
-    public double calculateGoalRate(int matchesPlayed) {
-        return (double) goals / matchesPlayed;
-    }
-}
-
-public class Main {
-    public static void main(String[] args) {
-        LaLigaPlayer messi = new LaLigaPlayer("Messi", 30, 1000000);
-        PremierLeaguePlayer ronaldo = new PremierLeaguePlayer("Ronaldo", 25, 900000);
-
-        
-        messi.displayBasicInfo();
-        System.out.println("League: LaLiga");
-        System.out.println("Goal Rate: " + messi.calculateGoalRate(35) + "\n");
-
-        
-        ronaldo.displayBasicInfo();
-        System.out.println("League: Premier League");
-        System.out.println("Goal Rate: " + ronaldo.calculateGoalRate(32));
     }
 }
